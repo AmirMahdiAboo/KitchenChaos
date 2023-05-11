0fd0b78d5b00a9b06d71cd9b038682c98e1c6ab4
AmirMahdi Abooshahab <amir.mahdi.aboo@gmail.com>
2023-05-11 16:46:30 +0330

    Player script created for movement in the script folder

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    [SerializeField] private float _moveSpeed = 7f;
+    private void Update()
+    {
+        Vector2 inputvector = new Vector2(0, 0);
+        if (Input.GetKey(KeyCode.W))
+        {
+            inputvector.y = +1;
+        }
+        if (Input.GetKey(KeyCode.S))
+        {
+            inputvector.y = -1;
+        }
+        if (Input.GetKey(KeyCode.A))
+        {
+            inputvector.x = -1;
+        }
+        if (Input.GetKey(KeyCode.D))
+        {
+            inputvector.x = +1;
+        }
+
+        inputvector = inputvector.normalized;
+        Vector3 moveDir = new Vector3(inputvector.x, 0, inputvector.y);
+        transform.position += moveDir * Time.deltaTime * _moveSpeed;
+
+    }
+}
