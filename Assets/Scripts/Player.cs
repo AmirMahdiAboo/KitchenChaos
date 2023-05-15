2ffc3e5d89b175c7826ed01c80d7c4651764c326
AmirMahdi Abooshahab <amir.mahdi.aboo@gmail.com>
2023-05-15 10:47:55 +0330

    Input system refactor done

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -6,30 +6,12 @@ public class Player : MonoBehaviour
 {
     [SerializeField] private float _moveSpeed = 7f;
     [SerializeField] private float _rotationSpeed = 10f;
-
+    [SerializeField] private GameInput _gameInput;
     private bool _isWalking;
     private void Update()
     {
-        Vector2 inputvector = new Vector2(0, 0);
-        if (Input.GetKey(KeyCode.W))
-        {
-            inputvector.y = +1;
-        }
-        if (Input.GetKey(KeyCode.S))
-        {
-            inputvector.y = -1;
-        }
-        if (Input.GetKey(KeyCode.A))
-        {
-            inputvector.x = -1;
-        }
-        if (Input.GetKey(KeyCode.D))
-        {
-            inputvector.x = +1;
-        }
-
-        inputvector = inputvector.normalized;
-        Vector3 moveDir = new Vector3(inputvector.x, 0, inputvector.y);
+       Vector2 inputVector = _gameInput.GetMovementVectorNormalized();
+        Vector3 moveDir = new Vector3(inputVector.x, 0, inputVector.y);
         transform.position += moveDir * Time.deltaTime * _moveSpeed;
 
         _isWalking = (moveDir != Vector3.zero);
