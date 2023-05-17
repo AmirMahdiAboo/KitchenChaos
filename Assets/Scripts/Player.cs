2445e3388b0d69dfba8fe7c78d0f966c12144426
AmirMahdi Abooshahab <amir.mahdi.aboo@gmail.com>
2023-05-17 10:49:55 +0330

    Collision Detection part is done !

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -10,9 +10,52 @@ public class Player : MonoBehaviour
     private bool _isWalking;
     private void Update()
     {
-       Vector2 inputVector = _gameInput.GetMovementVectorNormalized();
+        Vector2 inputVector = _gameInput.GetMovementVectorNormalized();
+
         Vector3 moveDir = new Vector3(inputVector.x, 0, inputVector.y);
-        transform.position += moveDir * Time.deltaTime * _moveSpeed;
+
+        float moveDistance = _moveSpeed * Time.deltaTime;
+        float playerRadius = 0.7f;
+        float playerHeight = 2f;
+        bool canMove = !Physics.CapsuleCast(transform.position, transform.position + Vector3.up * playerHeight, playerRadius, moveDir, moveDistance);
+
+        if(!canMove)
+        {
+            // Connot move towards the moveDir
+
+            // Attempts only the X movement
+            Vector3 moveDirX = new Vector3(moveDir.x, 0, 0).normalized;
+            canMove = !Physics.CapsuleCast(transform.position, transform.position + Vector3.up * playerHeight, playerRadius, moveDirX, moveDistance);
+
+            if(canMove)
+            {
+                // Can move only on the X
+                moveDir = moveDirX;
+            }
+            else
+            {
+                // Cannot move only on the X
+
+                //Attempt only Z movement
+                Vector3 moveDirZ = new Vector3(0,0,moveDir.z).normalized;
+                canMove = !Physics.CapsuleCast(transform.position, transform.position + Vector3.up * playerHeight, playerRadius, moveDirZ, moveDistance);
+
+                if (canMove)
+                {
+                    // Can only move on the Z
+                    moveDir = moveDirZ;
+                }
+                else
+                {
+                    // Cannot move in any direction
+                }
+            }
+        }
+
+        if (canMove)
+        {
+            transform.position += moveDir * moveDistance;
+        }
 
         _isWalking = (moveDir != Vector3.zero);
 
