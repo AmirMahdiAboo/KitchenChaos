aa765e5db0bf0f325cb0195ae5306ad0f8b78bae
AmirMahdi Abooshahab <amir.mahdi.aboo@gmail.com>
2023-05-18 12:23:51 +0330

    ContainerCounter part is done!

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -11,7 +11,7 @@ public class Player : MonoBehaviour , IKitchenObjectParent
     public event EventHandler<OnSelectedCounterChangedEventArgs> OnSelectedCounterChanged;
     public class OnSelectedCounterChangedEventArgs : EventArgs
     {
-        public ClearCounter selectedCounter;
+        public BaseCounter selectedCounter;
     }
 
     [SerializeField] private float _moveSpeed = 7f;
@@ -22,7 +22,7 @@ public class Player : MonoBehaviour , IKitchenObjectParent
 
     private bool _isWalking;
     private Vector3 lastInteractDir;
-    private ClearCounter selectedCounter;
+    private BaseCounter selectedCounter;
     private KitchenObject kitchenObject;
 
     private void Awake()
@@ -71,12 +71,12 @@ public class Player : MonoBehaviour , IKitchenObjectParent
         float interactDistance = 2f;
         if (Physics.Raycast(transform.position, lastInteractDir, out RaycastHit raycastHit, interactDistance, counterLayerMask))
         {
-            if (raycastHit.transform.TryGetComponent(out ClearCounter clearCounter))
+            if (raycastHit.transform.TryGetComponent(out BaseCounter baseCounter))
             {
                 // Has counter
-                if (clearCounter != selectedCounter)
+                if (baseCounter != selectedCounter)
                 {
-                    SetSelectedCounter(clearCounter);
+                    SetSelectedCounter(baseCounter);
                 }
             }
             else
@@ -143,7 +143,7 @@ public class Player : MonoBehaviour , IKitchenObjectParent
         transform.forward = Vector3.Slerp(transform.forward, moveDir, Time.deltaTime * _rotationSpeed);
     }
 
-    private void SetSelectedCounter(ClearCounter selectedCounter)  
+    private void SetSelectedCounter(BaseCounter selectedCounter)  
     {
         this.selectedCounter = selectedCounter;
 
